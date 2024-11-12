ce99fb2750889c0afa7d2073e82023d10aedb9c1
hariake <tosarv@gmail.com>
2024-11-12 22:34:37 +0200

    Header valmis

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
-import ProfilePicture from './ProfilePicture';  // Import the ProfilePicture component
+import { View, Image } from 'react-native';
+import ProfilePicture from './ProfilePicture';
+import Wealth from './Wealth';
+import Power from './Power';
 
 export default function Header() {
   return (
-    <View className="w-full h-72 bg-brown-700 p-4 flex flex-row items-center">
-      <ProfilePicture />
-      {/* Other header content can go here */}
+    <View className="w-full h-56 bg-brown-500 p-3 flex flex-row items-center">
+      <ProfilePicture className="w-36 h-36" />
+      <View className="ml-3 flex flex-col justify-between h-4/5">
+        <Wealth className="w-32 h-20" />
+        <Power className="w-32 h-20" />
+      </View>
+      <View className="ml-3 flex flex-col justify-between h-4/5">
+        <View className="w-20 h-20 bg-brown-700 rounded-xl flex justify-center items-center p-1">
+          <Image
+            source={require('../assets/images/shop.png')}
+            className="w-12 h-12 rounded-xl"
+          />
+        </View>
+        <View className="w-20 h-14 bg-brown-700 rounded-xl flex justify-center items-center p-1">
+          <Image
+            source={require('../assets/images/settings.png')}
+            className="w-12 h-12 rounded-xl"
+          />
+        </View>
+      </View>
     </View>
   );
 }
--- components/ProfilePicture.jsx
+++ components/ProfilePicture.jsx
@@ -4,12 +4,11 @@ import { View, Image } from 'react-native';
 const ProfilePicture = () => {
   return (
     <View
-      className="w-1/5 h-4/5 bg-brown-800 p-4 flex justify-center items-center"
-      style={{ marginLeft: '5%' }} // Adds padding from the left edge
+      className="w-44 bg-brown-700 rounded-xl h-4/5  ml-2 flex justify-center items-center" // Increased width and height
     >
       <Image
-        source={require('../assets/images/profile.png')} // Change path as needed
-        className="w-full h-full rounded-full"
+        source={require('../assets/images/profile.png')}
+        className="w-full h-full rounded-l"
       />
     </View>
   );

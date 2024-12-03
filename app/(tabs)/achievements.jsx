dc1c7d56410d4ca5b6abfa2d8f38797de9221610
gaor-jjj <kaurtamm1995@gmail.com>
2024-12-03 15:35:34 +0200

    Achievements view header and bg

--- app/(tabs)/achievements.jsx
+++ app/(tabs)/achievements.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, ImageBackground } from 'react-native'
 import React from 'react'
+import Header from '../../components/Header'
+import { images } from '../../constants'
 
 const achievements = () => {
   return (
-    <View>
-      <Text>achievements</Text>
-    </View>
+    <SafeAreaView className='flex-1'>
+      <View className='flex-1'>
+        <Header />
+        <View className="h-1/4">
+          <ImageBackground
+            source={images.achievementsbg}
+            resizeMode='cover'
+            className='flex-1 justify-center items-center'
+          >
+            <View>
+
+            </View>
+          </ImageBackground>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 

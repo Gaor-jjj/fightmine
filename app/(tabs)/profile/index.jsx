75f2205bcaec7fdc06926029233f5529d416e59c
gaor-jjj <kaurtamm1995@gmail.com>
2024-12-05 14:15:17 +0200

    Profile view finished

--- app/(tabs)/profile/index.jsx
+++ app/(tabs)/profile/index.jsx
@@ -1,12 +1,25 @@
-import { View, SafeAreaView } from 'react-native'
+import { View, SafeAreaView, Text, ImageBackground } from 'react-native'
 import React from 'react'
 import Header from '../../../components/Header'
+import { images } from '../../../constants'
+import ProfileComponent from '../../../components/ProfileComponent'
 
 const Profile = () => {
   return (
     <SafeAreaView className='flex-1 bg-neutral'>
       <View className='flex-1'>
         <Header />
+        <View className="h-1/4">
+          <ImageBackground
+            source={images.profilebg}
+            resizeMode='cover'
+            className='flex-1 justify-center items-center'
+          >
+          </ImageBackground>
+        </View>
+        <View className='flex-1 items-center p-4'>
+          <ProfileComponent></ProfileComponent>
+        </View>
       </View>
     </SafeAreaView>
   )

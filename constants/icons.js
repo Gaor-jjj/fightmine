dcf48b8862a3e8d4000317a4bab295f884761723
gaor-jjj <kaurtamm1995@gmail.com>
2024-12-05 13:27:22 +0200

    Finished achievements view

--- constants/icons.js
+++ constants/icons.js
@@ -3,11 +3,13 @@ import home from '../assets/icons/home.png'
 import fight from '../assets/icons/fight.png'
 import settings from '../assets/icons/settings.png'
 import goldtrophy from '../assets/icons/goldtrophy.png'
+import achievementicon from '../assets/icons/achievementicon.png'
 
 export default {
     achievements,
     home,
     fight,
     settings,
-    goldtrophy
+    goldtrophy,
+    achievementicon
 }

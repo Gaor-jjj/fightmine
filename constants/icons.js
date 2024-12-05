72766ac47e432ef91aa2a208611cf715173458ac
gaor-jjj <kaurtamm1995@gmail.com>
2024-12-05 13:47:51 +0200

    Shop view finished

--- constants/icons.js
+++ constants/icons.js
@@ -3,11 +3,13 @@ import home from '../assets/icons/home.png'
 import fight from '../assets/icons/fight.png'
 import settings from '../assets/icons/settings.png'
 import goldtrophy from '../assets/icons/goldtrophy.png'
+import shopicon from '../assets/icons/shopicon.png'
 
 export default {
     achievements,
     home,
     fight,
     settings,
-    goldtrophy
+    goldtrophy,
+    shopicon
 }
